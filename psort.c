fe7a01faaa0547baa0281eb0fce7ec27b907db15
Emily Berger <eaberger2@wisc.edu>
2022-11-03 17:16:33 -0500

    Added more to merge sort

--- psort.c
+++ psort.c
@@ -15,15 +15,41 @@
 #define THREAD_MAX get_nprocs()
 
 pthread_mutex_t lock;
+int part = 0;
 
-typedef struct file_data{
+typedef struct{
   int total_records;
   char *mapping;
 } f_data;
 
+
+void merge(int low, int mid, int high){
+
+}
+
+void merge_sort(int low, int high){
+  int mid = low + (high - low) / 2;
+  if(low < high){
+    merge_sort(low,mid);
+    merge_sort(mid+1,high);
+    merge(low,mid,high);
+  }
+}
+
 //https://www.geeksforgeeks.org/merge-sort-using-multi-threading/
 void* merge_sort(void* arg){
-
+  //split the address space by the the number of threads and have each thread merge sort its own section
+  //when threads finish sorting their section have one of the threads merge its section and another, keep going until the sections are all merged -> prob 
+  //need to use semaphore for this because the merge sort is producing two sorted sections and the consumer is merging these sections
+  int thread_part = part++;
+  int low = thread_part * (arg->total_records / THREAD_MAX); //index of first record in thread's section
+  int high = (thread_part + 1) * (arg->total_records / THREAD_MAX) - 1; //index of last record in thread's section
+  int mid = low + (high - low) + 2;
+  if(low < high){ //make sure there isn't only one record in the section
+    merge_sort(low, mid);
+    merge_sort(mid + 1, high);
+    merge(low, mid, high);
+  }
 }
 
 int main(int argc, char *argv[]){
